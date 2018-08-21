47c9dc629b953da1d422b1c9ad76b33a76952e8d
Ihor Breza <seniorigor@gmail.com>
2018-08-21 15:12:14 +0200

    Initial commit

---
+++ lib/real_page_calc.rb
@@ -0,0 +1,5 @@
+require "real_page_calc/version"
+
+module RealPageCalc
+  # Your code goes here...
+end
---
+++ spec/real_page_calc_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RealPageCalc do
+  it "has a version number" do
+    expect(RealPageCalc::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
